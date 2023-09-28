e026a5e1b7ab5e304385e7a9a49d1eb04902cf45
mcao516 <c_meng@outlook.com>
2023-09-27 17:43:28 -0700

    add data-preprocessing to train.sh

--- tasks/qa_feedback/train.sh
+++ tasks/qa_feedback/train.sh
@@ -1,9 +1,13 @@
-conda activate fgrlaif
-
 echo "========== SETTINGS =========="
 set -e
 DATA_FOLDER=toy
 OUTPUT_FOLDER_NAME=test
+
+
+echo "========== DATE PREPROCESSING =========="
+python tasks/qa_feedback/reward_modeling/create_rel_fact_rm_files.py --feedback_level subsentence --error_category NF-ERR --ignore_context --data_dir ./tasks/qa_feedback/${DATA_FOLDER}/
+python tasks/qa_feedback/reward_modeling/create_rel_fact_rm_files.py --feedback_level sentence --error_category F-ERR --data_dir ./tasks/qa_feedback/${DATA_FOLDER}/
+python tasks/qa_feedback/reward_modeling/create_comp_rm_files.py --input_dir ./tasks/qa_feedback/${DATA_FOLDER}/ --output_dir ./tasks/qa_feedback/${DATA_FOLDER}/COMP_sequence/
 
 
 echo "========== REL REWARD MODELING =========="
