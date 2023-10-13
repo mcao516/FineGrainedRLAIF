a64c03d81edf90eccbb2448d058ac06bb0494839
mcao610 <c_meng@outlook.com>
2023-10-13 14:34:31 -0400

    add evaluation script

--- tasks/qa_feedback/training/eval_finegrained.py
+++ tasks/qa_feedback/training/eval_finegrained.py
@@ -35,10 +35,11 @@ log = accelerate.logging.get_logger(__name__, log_level='INFO')
 def log_info(s):
     if accelerator.is_main_process:
         log.info(s)
-        
+
 # load parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", required=True, type=str, help="path to config file")
+parser.add_argument("--policy_ckpt", required=True, type=str, help="path to policy model checkpoint")
 parser.add_argument("--rm_rel_ckpt", required=True, type=str, help="path to relevance reward model checkpoint")
 parser.add_argument("--rm_fact_ckpt", required=True, type=str, help="path to factuality reward model checkpoint")
 parser.add_argument("--rm_comp_ckpt", required=True, type=str, help="path to completeness reward model checkpoint")
@@ -48,6 +49,7 @@ parser.add_argument("--folder_name", default="data", type=str)
 args = parser.parse_args()
 # load yaml file
 with open(args.config) as f:
+    policy_ckpt = args.policy_ckpt
     rm_rel_ckpt = args.rm_rel_ckpt
     rm_fact_ckpt = args.rm_fact_ckpt
     rm_comp_ckpt = args.rm_comp_ckpt
@@ -57,12 +59,16 @@ with open(args.config) as f:
     args =yaml.safe_load(f)
 
     # overwrite ckpt path
+    args['policy_model_to_eval'] = policy_ckpt
     args['reward']['relevance_model']['ckpt'] = rm_rel_ckpt
     args['reward']['factuality_model']['ckpt'] = rm_fact_ckpt
     args['reward']['completeness_model']['ckpt'] = rm_comp_ckpt
     args['logging']['save_dir'] = save_dir
     args['mean_std_file_path'] = mean_std_file_path
     args['folder_name'] = folder_name
+    args['logging']['wandb_project'] = "FineGrainedRLHF-Eval"
+    args['logging']['run_name'] = "evaluation"
+
 
 # prepare data
 class TextGenDataset(Dataset):
@@ -162,134 +168,182 @@ def read_mean_std(file_path):
     return mean, std
 
 
-def main():
-    # read mean and std
-    mean, std = read_mean_std(args['mean_std_file_path'])
-    args['reward']['completeness_model']['mean'] = mean
-    args['reward']['completeness_model']['std'] = std
-    print("- mean: ", mean)
-    print("- std: ", std)
+def valid(args, accelerator, eval_dataloader, policy_model, reward_model):
+    accelerator.wait_for_everyone()
+    policy_model.model.eval()
 
+    columns=["step", "inputs", "outputs"]
+    wandb_table = None
+    
+    n_entries = 0
+    
+    with torch.no_grad():
+        for i, batch in enumerate(tqdm(eval_dataloader) if accelerator.is_main_process else eval_dataloader):
+
+            results = policy_model.sample(
+                prompts_input_ids=batch['prompts_input_ids'],
+                prompts_attention_mask=batch['prompts_attention_mask'],
+                **args['model']['policy_model']['eval_generation_kwargs'],
+            )
+
+            eval_results = reward_model.eval_metrics(
+                prompts_input_ids=results['prompts_input_ids'],
+                prompts_attention_mask=results['prompts_attention_mask'],
+                generated_input_ids=results['generated_input_ids'],
+                generated_attention_mask=results['generated_attention_mask'],
+                generated_texts=results['generated_text'],
+                metadata = batch['metadata'],
+            )
+            
+            # gather all results
+            batch = accelerator.gather_for_metrics(batch)
+            results = accelerator.gather_for_metrics(results)
+            
+            for eval_k, eval_v in eval_results.items():
+                eval_results[eval_k] = accelerator.gather(
+                    torch.tensor(eval_v, device=results['generated_input_ids'].device))
+                
+            # initialize wandb table if it does not exist
+            if wandb_table is None:
+                columns.extend(list(eval_results.keys())) 
+                wandb_table = wandb.Table(columns=columns)
+            
+            if accelerator.is_main_process: 
+                prompt_inputs = policy_model.tokenizer.batch_decode(results['prompts_input_ids'],
+                                                                skip_special_tokens=True, 
+                                                                clean_up_tokenization_spaces=True)
+                generated_texts = policy_model.tokenizer.batch_decode(results['generated_input_ids'],
+                                                                skip_special_tokens=True, 
+                                                                clean_up_tokenization_spaces=True)
+                
+                this_data_batch_size = results['prompts_input_ids'].shape[0]
+                this_lens = torch.sum(results['generated_attention_mask'], dim=-1)
+                
+                for batch_i in range(this_data_batch_size):
+                    this_entry = [0, prompt_inputs[batch_i], generated_texts[batch_i]]
+                    
+                    for eval_v in eval_results.values():
+                        this_entry.append(eval_v[batch_i].item())
+                    
+                    wandb_table.add_data(*this_entry)
+                    n_entries += 1
+
+    if accelerator.is_main_process:        
+        # stats = {'eval/step': 0, f'eval_generation/step_{0}': wandb_table}
+        stats = {'eval/step': 0}
+
+        value_columns = columns[3:] # the first three are steps, inputs, outputs
+        stats.update(reward_model.aggregate_metrics(wandb_table, value_columns))
+
+        if args['logging']['wandb_log']:
+            wandb.log(stats)
+
+        for k, v in stats.items():
+            log_info(f'Evaluated [{k}] = {v:.4f}')
+
+        print("##### Table #####")
+        print(wandb_table)
+
+
+def main():
     # set seed
     set_seed(args['train']['seed'], args['train']['cuda_deterministic'])
-    
+
     # set saving directories
     log_info(f"Write to output directory: {args['logging']['save_dir']}")
-    
+
     if accelerator.is_main_process:
         ensure_dir(args['logging']['save_dir'])
         # save the config file
         with open(os.path.join(args['logging']['save_dir'], 'args.json'), 'w') as f:
             json.dump(args, f, indent=2)
 
-    
     # initialize policy and value model tokenizers
     tokenizer = transformers.AutoTokenizer.from_pretrained(args['model']['policy_model']['ckpt'], 
                                                            model_max_length=args['env']['max_input_len'])
     tokenizer.padding_side = args['model']['policy_model']['input_padding_side']
     tokenizer.max_input_len = args['env']['max_input_len']
     tokenizer.max_generated_len = args['env']['max_generated_len']
-    
-    
+
     # Load data
-    log_info(f'Loading data ...')
-    train_dataset = TextGenDataset( 'train', tokenizer, accelerator=accelerator, folder_name=args['folder_name'])
-    # train ds is shuffled in its constructor
-    train_dataloader = DataLoader(train_dataset, batch_size=args['train']['sampling_batch_size_per_card'], 
-                                  shuffle=False, drop_last=True, collate_fn=train_dataset.collate_fn)
-
-    eval_dataset = TextGenDataset( 'dev',  tokenizer, accelerator=accelerator, length_limit=None, folder_name=args['folder_name'])
-    eval_dataloader = DataLoader(eval_dataset, batch_size=args['train']['sampling_batch_size_per_card'], 
+    log_info(f'Loading evaluation data ...')
+    eval_dataset = TextGenDataset('dev',  tokenizer, accelerator=accelerator, length_limit=None)
+    eval_dataloader = DataLoader(eval_dataset, batch_size=16,
                                  shuffle=False, drop_last=False, collate_fn=eval_dataset.collate_fn)
-
-    train_dataloader, eval_dataloader = accelerator.prepare(train_dataloader, eval_dataloader)
-
+    eval_dataloader = accelerator.prepare(eval_dataloader)
 
     # Initialize models and optimizer
     log_info(f'Initializing models ...')
-
-    ref_policy = T5Policy(
-        model_ckpt=args['model']['policy_model']['ckpt'],
-        tokenizer=tokenizer,
-        policy_value_sharing=args['model']['value_model']['policy_value_sharing'],
-        accelerator=accelerator,
-    )
-    ref_policy.model, ref_policy.linear = accelerator.prepare(ref_policy.model, ref_policy.linear)
-    policy = T5Policy(
-        model_ckpt=args['model']['policy_model']['ckpt'],
-        tokenizer=tokenizer,
-        policy_value_sharing=args['model']['value_model']['policy_value_sharing'],
-        accelerator=accelerator,
-    )
-    policy.model, policy.linear = accelerator.prepare(policy.model, policy.linear)
-    
-    value = T5Value(
-        model_ckpt=args['model']['value_model']['ckpt'],
-        model=policy.model if args['model']['value_model']['policy_value_sharing'] else None,
-        tokenizer=tokenizer,
-        accelerator=accelerator,
-        freeze_model=False if args['model']['value_model']['policy_value_sharing'] else args['model']['value_model']['freeze_value_model'],
+    if os.path.isfile(args['policy_model_to_eval']) and \
+        not os.path.isdir(args['policy_model_to_eval']):
+        log_info(f"Load policy model from: {args['policy_model_to_eval']}")
+        policy = T5Policy(
+            model_ckpt=args['model']['policy_model']['ckpt'],
+            tokenizer=tokenizer,
+            policy_value_sharing=args['model']['value_model']['policy_value_sharing'],
+            accelerator=accelerator,
         )
-    if not args['model']['value_model']['policy_value_sharing']:
-        value.model, value.linear = accelerator.prepare(value.model, value.linear)
-    
+        policy.model.load_state_dict(torch.load(args['policy_model_to_eval']))
+    else:
+        log_info(f"Load policy model from: {args['policy_model_to_eval']}")
+        policy = T5Policy(
+            model_ckpt=args['policy_model_to_eval'],
+            tokenizer=tokenizer,
+            policy_value_sharing=args['model']['value_model']['policy_value_sharing'],
+            accelerator=accelerator,
+        )
+    policy.model, policy.linear = accelerator.prepare(policy.model, policy.linear)
+
     reward = FineGrainedReward(
         tokenizer=tokenizer,
         non_factual_model_ckpt=args['reward']['relevance_model']['ckpt'],
         factual_model_ckpt=args['reward']['factuality_model']['ckpt'],
         completeness_model_ckpt=args['reward']['completeness_model']['ckpt'],
         kl_coef=args['ppo']['kl_coef'],
-        verbosity_positive_reward = args['reward']['relevance_model']['positive_reward'],
-        verbosity_negative_reward = args['reward']['relevance_model']['negative_reward'],
-        factuality_positive_reward = args['reward']['factuality_model']['positive_reward'],
-        factuality_negative_reward = args['reward']['factuality_model']['negative_reward'],
-        completeness_reward_mean = args['reward']['completeness_model']['mean'],
-        completeness_reward_std = args['reward']['completeness_model']['std'],
-        completeness_reward_bias = args['reward']['completeness_model']['bias'],
-        completeness_reward_scale = args['reward']['completeness_model']['scale'],
-        sep = "</s>"
+        verbosity_positive_reward=args['reward']['relevance_model']['positive_reward'],
+        verbosity_negative_reward=args['reward']['relevance_model']['negative_reward'],
+        factuality_positive_reward=args['reward']['factuality_model']['positive_reward'],
+        factuality_negative_reward=args['reward']['factuality_model']['negative_reward'],
+        completeness_reward_mean=args['reward']['completeness_model']['mean'],
+        completeness_reward_std=args['reward']['completeness_model']['std'],
+        completeness_reward_bias=args['reward']['completeness_model']['bias'],
+        completeness_reward_scale=args['reward']['completeness_model']['scale'],
+        sep="</s>"
     )
     
     # prepare reward models
     reward.verbosity_reward.nf_reward_model = accelerator.prepare(reward.verbosity_reward.nf_reward_model)
     reward.factuality_reward.f_reward_model = accelerator.prepare(reward.factuality_reward.f_reward_model)
     reward.completeness_reward.model = accelerator.prepare(reward.completeness_reward.model)
-    
-    # prepare optimizers and schedulers
-    # if args['model']['value_model']['policy_value_sharing']:
-    #     parameters = chain(policy.model.parameters(), policy.linear.parameters())
-    # else:
-    #     parameters = chain(policy.model.parameters(), policy.linear.parameters(), value.model.parameters(), value.linear.parameters())
-    # optimizer = torch.optim.Adam(parameters, lr=args['train']['lr'], eps=1e-5)
-    # total_steps = ceil_div(args['train']['total_episodes'], 
-    #                             args['train']['sampling_batch_size_per_card'] * accelerator.num_processes * args['env']['train_num_samples_per_input'])
-    
-    # scheduler = transformers.get_scheduler(
-    #     name='linear',
-    #     optimizer=optimizer,
-    #     num_warmup_steps=100*args['train']['n_ppo_epoch_per_rollout']*accelerator.num_processes,
-    #     num_training_steps=total_steps*args['train']['n_ppo_epoch_per_rollout']*accelerator.num_processes,
-    # )
-    
-    # optimizer, scheduler = accelerator.prepare(optimizer, scheduler)
-
 
     # Set up trainer
-    trainer = PPOTrainer(
-        args=args,
-        train_dataloader=train_dataloader,
-        eval_dataloader=eval_dataloader,
-        ref_policy_model=ref_policy,
-        policy_model=policy,
-        value_model=value,
-        reward_model=reward,
-        optimizer=None,
-        scheduler=None,
-        accelerator=accelerator,
-        log_info=log_info,
-    )
-    trainer.valid(0)
+    # trainer = PPOTrainer(
+    #     args=args,
+    #     train_dataloader=train_dataloader,
+    #     eval_dataloader=eval_dataloader,
+    #     ref_policy_model=None,
+    #     policy_model=policy,
+    #     value_model=None,
+    #     reward_model=reward,
+    #     optimizer=None,
+    #     scheduler=None,
+    #     accelerator=accelerator,
+    #     log_info=log_info,
+    # )
 
+    if accelerator.is_main_process:
+        if args['logging']['wandb_log']:
+            wandb.init(entity=args["logging"]["wandb_entity"], project=args["logging"]["wandb_project"], name=args['logging']['run_name'], config=args)
+        else:
+            wandb.init(config=args, mode='disabled')
 
+        wandb.define_metric('train/step')
+        wandb.define_metric('eval/step')
+        wandb.define_metric('train/*', step_metric='train/step')
+        wandb.define_metric('eval/*', step_metric='eval/step', summary='max')
+
+    valid(args, accelerator, eval_dataloader, policy, reward)
+
+            
 if __name__ == '__main__':
     main()
--- tasks/qa_feedback/training/eval_finegrained.sh
+++ tasks/qa_feedback/training/eval_finegrained.sh
@@ -1,25 +1,29 @@
-DATA_FOLDER=llm340bmix_data
-OUTPUT_FOLDER_NAME=llm340bmix_data
+DATA_FOLDER=data
+OUTPUT_FOLDER_NAME=data
 
 echo $DATA_FOLDER
 echo $OUTPUT_FOLDER_NAME
 
-REL_OUTPUT_DIR=./tasks/qa_feedback/model_outputs/${OUTPUT_FOLDER_NAME}/rel_rm
-FACT_OUTPUT_DIR=./tasks/qa_feedback/model_outputs/${OUTPUT_FOLDER_NAME}/fact_rm
-COMP_OUTPUT_DIR=./tasks/qa_feedback/model_outputs/${OUTPUT_FOLDER_NAME}/comp_rm
+POLICY_OUTPUT_DIR=$HOME/FineGrainedRLAIF/tasks/qa_feedback/model_outputs/data2/fine_grained/best_policy.pth
+POLICY_OUTPUT_DIR=$HOME/FineGrainedRLHF-Original/tasks/qa_feedback/model_outputs/llm_data/fine_grained/best_policy.pth
+
+REL_OUTPUT_DIR=$HOME/FineGrainedRLAIF/fgrlhf_models/rel_rm
+FACT_OUTPUT_DIR=$HOME/FineGrainedRLAIF/fgrlhf_models/fact_rm
+COMP_OUTPUT_DIR=$HOME/FineGrainedRLAIF/fgrlhf_models/comp_rm
 
 accelerate launch \
-    --main_process_port 29500 \
+    --main_process_port 8899 \
     --num_machines 1 \
     --machine_rank 0 \
     --num_processes 4 \
     --mixed_precision bf16 \
     --multi_gpu \
-    tasks/qa_feedback/training/eval_finegrained.py \
-        --config tasks/qa_feedback/training/fine_grained_config.yml \
-        --rm_rel_ckpt $REL_OUTPUT_DIR \
-        --rm_fact_ckpt $FACT_OUTPUT_DIR \
-        --rm_comp_ckpt $COMP_OUTPUT_DIR \
-        --mean_std_file_path $COMP_OUTPUT_DIR/mean_std.txt \
-        --save_dir ./tasks/qa_feedback/model_outputs/${OUTPUT_FOLDER_NAME}/fine_grained/ \
-        --folder_name $DATA_FOLDER;
+        tasks/qa_feedback/training/eval_finegrained.py \
+            --config tasks/qa_feedback/training/fine_grained_config.yml \
+            --policy_ckpt $POLICY_OUTPUT_DIR \
+            --rm_rel_ckpt $REL_OUTPUT_DIR \
+            --rm_fact_ckpt $FACT_OUTPUT_DIR \
+            --rm_comp_ckpt $COMP_OUTPUT_DIR \
+            --mean_std_file_path $COMP_OUTPUT_DIR/mean_std.txt \
+            --save_dir ./eval \
+            --folder_name $DATA_FOLDER;
